c84ea37f459823e577f2e14847213b6a1e12dde4
quyennguyencoder <quyenn0301@ut.edu.vn>
2026-05-21 16:57:39 +0700

    new

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import { useAuthStore } from '../stores/useAuthStore'
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
@@ -7,31 +6,3 @@ export const axiosInstance = axios.create({
     'Content-Type': 'application/json',
   },
 })
-
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const { accessToken } = useAuthStore.getState()
-    if (accessToken && config.headers) {
-      config.headers.Authorization = `Bearer ${accessToken}`
-    }
-    return config
-  },
-  (error) => {
-    return Promise.reject(error)
-  }
-)
-
-axiosInstance.interceptors.response.use(
-  (response) => {
-    return response
-  },
-  async (error) => {
-    const originalRequest = error.config
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true
-      // You can implement refresh token logic here if needed
-      useAuthStore.getState().logout()
-    }
-    return Promise.reject(error)
-  }
-)
